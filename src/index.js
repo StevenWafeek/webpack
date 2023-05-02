77212961c2ecc9186b0cb4521316d92825a36097
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-05-02 16:44:23 +0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import _ from 'lodash';
 import printMe from './print.js';
 
- function component() {
-   const element = document.createElement('div');
+function component() {
+  const element = document.createElement('div');
   const btn = document.createElement('button');
 
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
 
   element.appendChild(btn);
 
-   return element;
- }
+  return element;
+}
 
- document.body.appendChild(component());
+document.body.appendChild(component());
